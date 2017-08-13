313a16bf60e447960f4b89609e3674a6bf23be27
sebastianplesciuc <plesciuc.sebastian@gmail.com>
2017-08-13 12:04:23 +0300

    [WIP] Added interface for value types and equals comparison.

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,42 +1,51 @@
 package linkedlist
 
-// LinkedList interface
-type LinkedList interface {
-	Append(data string)
+// Value is the interface a value needs to implements to be stored in the linked list
+type Value interface {
+	Equals(Value) (bool, error)
+}
+
+// List interface wraps and exports the listed list object
+type List interface {
+	Append(data Value)
 
 	Begin() Iterator
 	End() Iterator
 }
 
-// Iterator bla bla
+// Iterator is actually a wrapper for a linked list node
 type Iterator interface {
-	Get() string
+	Get() Value
 	Next() Iterator
 	Previous() Iterator
 }
 
-type node struct {
-	data string
+// listNode is the struct for a linked list node
+type listNode struct {
+	data Value
 
-	next     *node
-	previous *node
+	next     *listNode
+	previous *listNode
 
 	// I hate this. This might violate the D in SOLID
 	parentList *listContainer
 }
 
+// listContainer is the struct for the linked list (parent object)
 type listContainer struct {
-	first *node
-	last  *node
+	first *listNode
+	last  *listNode
 
-	lastIterator *node
+	lastIterator *listNode
 }
 
-func (n *node) Get() string {
+// Get returns the Value stored in a node
+func (n *listNode) Get() Value {
 	return n.data
 }
 
-func (n *node) Next() Iterator {
+// Next returns the reference to the next node or the end interator
+func (n *listNode) Next() Iterator {
 	if n.next == nil {
 		return n.parentList.lastIterator
 	}
@@ -44,21 +53,26 @@ func (n *node) Next() Iterator {
 	return n.next
 }
 
-func (n *node) Previous() Iterator {
+// Previous returns the reference to the previous node
+func (n *listNode) Previous() Iterator {
 	return n.previous
 }
 
+// Begin returns a reference to the first node in the list
 func (ll *listContainer) Begin() Iterator {
 	return ll.first
 }
 
+// End returns a reference to the last node in the list
 func (ll *listContainer) End() Iterator {
 	return ll.lastIterator
 }
 
-func (ll *listContainer) Append(data string) {
+// Append adds an node to the end of the list
+func (ll *listContainer) Append(data Value) {
+	// If this is the first value, create the first node
 	if ll.first == nil {
-		ll.first = &node{
+		ll.first = &listNode{
 			data:       data,
 			next:       nil,
 			previous:   nil,
@@ -66,7 +80,9 @@ func (ll *listContainer) Append(data string) {
 		}
 
 		ll.last = ll.first
-		ll.lastIterator = &node{
+
+		// The last iterator needs to be an element without data that represents End()
+		ll.lastIterator = &listNode{
 			next:       nil,
 			previous:   ll.last,
 			parentList: ll,
@@ -75,27 +91,28 @@ func (ll *listContainer) Append(data string) {
 		return
 	}
 
-	finder := ll.first
-	for finder.next != nil {
-		finder = finder.next
-	}
+	/*
+		finder := ll.first
+		for finder.next != nil {
+			finder = finder.next
+		}*/
 
-	finder.next = &node{
+	ll.last.next = &listNode{
 		data:       data,
-		previous:   finder,
+		previous:   ll.last,
 		next:       nil,
 		parentList: ll,
 	}
 
-	ll.last = finder.next
-	ll.lastIterator = &node{
+	ll.last = ll.last.next
+	ll.lastIterator = &listNode{
 		next:       nil,
 		previous:   ll.last,
 		parentList: ll,
 	}
 }
 
-// New returns a LinkedList
-func New() LinkedList {
+// New is the factory function for a linked List
+func New() List {
 	return &listContainer{}
 }
--- main.go
+++ main.go
@@ -6,15 +6,42 @@ import (
 	"github.com/sebastianplesciuc/golang-data-structures/linkedlist"
 )
 
+type myData struct {
+	data string
+}
+
+func (m *myData) Equals(otherValue linkedlist.Value) (bool, error) {
+	other, ok := otherValue.(*myData)
+	if !ok {
+		return false, fmt.Errorf("Invalid type")
+	}
+	return other.data == m.data, nil
+}
+
 func main() {
 	list := linkedlist.New()
 
-	list.Append("data1")
-	list.Append("data2")
+	d1 := &myData{data: "data1"}
+	d2 := &myData{data: "data2"}
+
+	list.Append(d1)
+	list.Append(d2)
+	list.Append(d2)
 
 	iterator := list.Begin()
 	for iterator != list.End() {
-		fmt.Println(iterator.Get())
+		dataVal, ok := iterator.Get().(*myData)
+		if !ok {
+			panic("Unexpected data type in list")
+		}
+
+		equals, err := iterator.Get().Equals(d2)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Printf("%s == %s : %v \n", dataVal.data, d2.data, equals)
+
 		iterator = iterator.Next()
 	}
 
